bf83adee56dcca4ab7e4a0cfc8aead96885ad49f
Raja CSP <raja.csp@gmail.com>
2019-06-09 01:59:04 -0400

    base updated

--- src/main/java/org/tact/base/rest/BaseController.java
+++ src/main/java/org/tact/base/rest/BaseController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/base")
+@RequestMapping(value = "/")
 public class BaseController {
 	
     @RequestMapping(value = "", method = RequestMethod.GET)
